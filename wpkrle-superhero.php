6b250c45f47407c1e004343267a8066627a28545
Ivan Krstic <ikrstic.nis@gmail.com>
2019-02-25 11:44:02 +0100

    Added main plugin file with plugin header comment

---
+++ wpkrle-superhero.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Superheroes
+ * Plugin URI:  https://github.com/Kr1e/wpkrle-hero.git
+ * Description: Simple WordPress Plugin that creates Superhero post type for entering and classifying Superheroes
+ * Version:     1.0.0
+ * Author:      Ivan Krstic
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wpkrle-superhero
+ * Domain Path: /languages
+ */
+
+//If called directly abort
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
